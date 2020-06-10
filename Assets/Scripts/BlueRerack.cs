c45d49736eec67c021f83d726c18ce3422b99406
Joon Jang <w.joon.jang@gmail.com>
2020-06-10 11:34:05 -0700

    Revert "Revert "done""
    This reverts commit 3a89678686c556da017fd611b174defa85fb7e20.

--- Assets/Scripts/BlueRerack.cs
+++ Assets/Scripts/BlueRerack.cs
@@ -39,6 +39,9 @@ public class BlueRerack : MonoBehaviour
     [SerializeField]
     private Canvas[] WinUI;
 
+    public GameObject panel;
+    Animator animator;
+
     string cupColor;
     // Start is called before the first frame update
     void Start()
@@ -125,9 +128,14 @@ public class BlueRerack : MonoBehaviour
 
     void BlueWin()
     {
-        foreach (var tmp in WinUI)
+        //foreach (var tmp in WinUI)
+        //{
+        //    tmp.enabled = true;
+        //}
+        animator = panel.GetComponent<Animator>();
+        if (animator != null)
         {
-            tmp.enabled = true;
+            animator.SetBool("RedWins", true);
         }
     }
 
--- Assets/Scripts/Restart.cs
+++ Assets/Scripts/Restart.cs
@@ -5,11 +5,27 @@ using UnityEngine.SceneManagement;
 
 public class Restart : MonoBehaviour
 {
+	public GameObject panel;
 	public void RestartScene()
 	{
+		Animator animator = panel.GetComponent<Animator>();
+		if (animator != null)
+		{
+			animator.SetBool("Open", false);
+		}
+
 		CameraController.camBool = true;
-		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+		SwipeScript.touchEnabled = false;
+		SwipeScript.GameStart = false;
+		//SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+		StartCoroutine(RestartDelay());
 		Debug.Log("Clicked");
+	}
+
+	IEnumerator RestartDelay()
+    {
+		yield return new WaitForSeconds(2);
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
 	}
 
 
